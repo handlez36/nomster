7d5c1999b935bd6044b2a772e1daa54d00935fbf
Brandon <handlez36@gmail.com>
2015-10-21 21:35:15 +0000

    Installed Devise Gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Nomster::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Added for Devise gem
+  config.action_mailer.default_url_options = { :host => 'nomster-brandon-mclean.herokuapp.com' }
+  
   # Code is not reloaded between requests.
   config.cache_classes = true
 
