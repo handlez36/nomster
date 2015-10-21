0dbe2581f9ded12891fc63bc6d105ba19f79b059
Brandon <handlez36@gmail.com>
2015-10-21 00:04:26 +0000

    Added pagination to the Index.html.erb page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,9 @@
 class PlacesController < ApplicationController
     def index
-      @places = Place.all
+      # @places = Place.all
+      
+      # Paginated db pull
+      @places = Place.paginate( :page => params[:page], :per_page => 5 )
+      #@places = Place.page( params[:page] )
     end
 end
