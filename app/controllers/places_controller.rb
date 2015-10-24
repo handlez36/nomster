279598bfda65ee2090d830644992d329527ad333
Brandon <handlez36@gmail.com>
2015-10-24 03:42:52 +0000

    Locked down delete action

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,6 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, :only => [:new, :create]
+  # Requires users to be authenticated for the action values of the only key
+  before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
   
   def index
     # Paginated db pull
@@ -21,17 +22,31 @@ class PlacesController < ApplicationController
   
   def edit
     @place = Place.find(params[:id])
+    
+    if @place.user != current_user
+      return render :text => 'Not Allowed', :status => :forbidden
+    end
   end
   
   def update
     @place = Place.find(params[:id])
-    @place.update_attributes(place_params)
     
+    if @place.user != current_user
+      return render :text => 'Not Allowed', :status => :forbidden
+    end
+    
+    @place.update_attributes(place_params)  
+  
     redirect_to root_path
   end
   
   def destroy
     @place = Place.find(params[:id])
+    
+    if @place.user != current_user
+      return render :text => 'Not Allowed', :status => :forbidden
+    end
+    
     @place.destroy
     
     redirect_to root_path
