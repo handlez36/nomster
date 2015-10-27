0a54391546544e992ab8145a9e8ee852db7e8bfc
Brandon <handlez36@gmail.com>
2015-10-27 01:52:27 +0000

    Added ActiveRecord Callback to send an email after creating a new comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :place
+  after_create :send_comment_email
   
   # The hash key is what the user will see
   # The has value is what will be stored in the db
@@ -15,4 +16,9 @@ class Comment < ActiveRecord::Base
   def humanized_rating
     RATINGS.invert[self.rating]
   end
+  
+  def send_comment_email
+    NotificationMailer.comment_added(self).deliver
+  end
+  
 end
