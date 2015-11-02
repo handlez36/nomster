6d4ae2ecd6ed09fd81f1a5104d5f535a88dc1b71
Brandon <handlez36@gmail.com>
2015-11-02 18:35:57 +0000

    Re-added email send capability that works with unit and functional tests

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :place
-  #after_create :send_comment_email
+  after_create :send_comment_email
   
   # The hash key is what the user will see
   # The has value is what will be stored in the db
